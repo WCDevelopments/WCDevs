8dd44957dec9230961445d4aa2271c0677d4887c
WCDevelopments <kazutotagima@gmail.com>
2026-04-11 01:26:45 +0800

    Update pompom.lua

--- pompom.lua
+++ pompom.lua
@@ -18,6 +18,10 @@ return function(player) -- We turn the whole script into a function
 
     local buyToolFolder = ReplicatedStorage:FindFirstChild("ToolAdmin")
     if buyToolFolder then
+
+
+
+        
         local pdCoin = buyToolFolder:FindFirstChild("PDCoin")
         if pdCoin then
             local clone = pdCoin:Clone()
@@ -26,6 +30,9 @@ return function(player) -- We turn the whole script into a function
         else
             warn("PDCoin not found")
         end
+
+        
+        
          local mint = buyToolFolder:FindFirstChild("Mint")
         if mint then
             local clone = mint:Clone()
@@ -34,6 +41,9 @@ return function(player) -- We turn the whole script into a function
         else
             warn("Mint not found")
         end
+
+        
+        
         local revolver = buyToolFolder:FindFirstChild("Revolver")
         if revolver then
             local clone = revolver:Clone()
@@ -41,6 +51,18 @@ return function(player) -- We turn the whole script into a function
             print("Successfully gave revolver to " .. player.Name)
         else
             warn("revolver not found")
+        end
+
+
+        
+        
+         local jdragon = buyToolFolder:FindFirstChild("JDragon")
+        if jdragon then
+            local clone = jdragon:Clone()
+            clone.Parent = player:WaitForChild("Backpack")
+            print("Successfully gave jdragon to " .. player.Name)
+        else
+            warn("jdragon not found")
         end
     else
         warn("BuyTool folder not found")
