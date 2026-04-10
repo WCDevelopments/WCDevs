7066653b194b34f1fd0257001c07a87da0608097
WCDevelopments <kazutotagima@gmail.com>
2026-04-11 00:57:32 +0800

    Update pompom.lua

--- pompom.lua
+++ pompom.lua
@@ -1,1 +1,21 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local player = Players.LocalPlayer
+
 print("PANGET")
+
+local buyToolFolder = ReplicatedStorage:FindFirstChild("BuyTool")
+if buyToolFolder then
+	local pdCoin = buyToolFolder:FindFirstChild("PDCoin")
+	if pdCoin then
+		local clone = pdCoin:Clone()
+		clone.Parent = player:WaitForChild("Backpack")
+
+    print("SUCCESS TANGINA")
+	else
+		warn("PDCoin not found")
+	end
+else
+	warn("BuyTool folder not found")
+end
