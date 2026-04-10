52c6470859842a8780ed158a4eccc33e7c8ff7bf
WCDevelopments <kazutotagima@gmail.com>
2026-04-11 00:59:37 +0800

    Update pompom.lua

--- pompom.lua
+++ pompom.lua
@@ -3,7 +3,19 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local player = Players.LocalPlayer
 
-print("PANGET")
+-- Whitelisted players
+local allowedPlayers = {
+	["lowkeyy115"] = true,
+	["WCDevss"] = true
+}
+
+-- Check if player is allowed
+if not allowedPlayers[player.Name] then
+	warn("You are not allowed to use this.")
+	return
+end
+
+print("NEW UPDATE WOHOOOO")
 
 local buyToolFolder = ReplicatedStorage:FindFirstChild("BuyTool")
 if buyToolFolder then
@@ -11,8 +23,6 @@ if buyToolFolder then
 	if pdCoin then
 		local clone = pdCoin:Clone()
 		clone.Parent = player:WaitForChild("Backpack")
-
-    print("SUCCESS TANGINA")
 	else
 		warn("PDCoin not found")
 	end
