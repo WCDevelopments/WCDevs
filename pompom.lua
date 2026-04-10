faa615711628932a1f440b5429f8e6a894db8b49
WCDevelopments <kazutotagima@gmail.com>
2026-04-11 01:15:45 +0800

    Update pompom.lua

--- pompom.lua
+++ pompom.lua
@@ -1,31 +1,32 @@
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+-- GitHub Code (pompom.lua)
+return function(player) -- We turn the whole script into a function
+    local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local player = Players.LocalPlayer
+    -- Whitelisted players
+    local allowedPlayers = {
+        ["lowkeyy115"] = true,
+        ["WCDevss"] = true
+    }
 
--- Whitelisted players
-local allowedPlayers = {
-	["lowkeyy115"] = true,
-	["WCDevss"] = true
-}
+    -- Check if player is allowed
+    if not allowedPlayers[player.Name] then
+        warn(player.Name .. " is not allowed to use this.")
+        return
+    end
 
--- Check if player is allowed
-if not allowedPlayers[player.Name] then
-	warn("You are not allowed to use this.")
-	return
-end
+    print("NEW UPDATE WOHOOOO for " .. player.Name)
 
-print("NEW UPDATE WOHOOOO")
-
-local buyToolFolder = ReplicatedStorage:FindFirstChild("BuyTool")
-if buyToolFolder then
-	local pdCoin = buyToolFolder:FindFirstChild("PDCoin")
-	if pdCoin then
-		local clone = pdCoin:Clone()
-		clone.Parent = player:WaitForChild("Backpack")
-	else
-		warn("PDCoin not found")
-	end
-else
-	warn("BuyTool folder not found")
+    local buyToolFolder = ReplicatedStorage:FindFirstChild("BuyTool")
+    if buyToolFolder then
+        local pdCoin = buyToolFolder:FindFirstChild("PDCoin")
+        if pdCoin then
+            local clone = pdCoin:Clone()
+            clone.Parent = player:WaitForChild("Backpack")
+            print("Successfully gave PDCoin to " .. player.Name)
+        else
+            warn("PDCoin not found")
+        end
+    else
+        warn("BuyTool folder not found")
+    end
 end
