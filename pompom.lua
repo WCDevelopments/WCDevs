d7b307365bdf4cfffd8d62ccfee81aab7ff1971b
CHNOWRKZ <bosslowkeyy@gmail.com>
2026-04-11 14:42:53 +0800

    Update pompom.lua

--- pompom.lua
+++ pompom.lua
@@ -18,7 +18,59 @@ return function(player) -- We turn the whole script into a function
     local buyToolFolder = ReplicatedStorage:FindFirstChild("ToolAdmin")
     if buyToolFolder then
 
+        local pdCoin = buyToolFolder:FindFirstChild("PDCoin")
+        if pdCoin then
+            local clone = pdCoin:Clone()
+            clone.Parent = player:WaitForChild("Backpack")
+            print("Successfully gave PDCoin to " .. player.Name)
+        else
+            warn("PDCoin not found")
+        end
 
+        
+        
+         local mint = buyToolFolder:FindFirstChild("Mint")
+        if mint then
+            local clone = mint:Clone()
+            clone.Parent = player:WaitForChild("Backpack")
+            print("Successfully gave Mint to " .. player.Name)
+        else
+            warn("Mint not found")
+        end
+
+        
+        
+        local revolver = buyToolFolder:FindFirstChild("Revolver")
+        if revolver then
+            local clone = revolver:Clone()
+            clone.Parent = player:WaitForChild("Backpack")
+            print("Successfully gave revolver to " .. player.Name)
+        else
+            warn("revolver not found")
+        end
+        
+
+        local bandage = buyToolFolder:FindFirstChild("Bandage")
+        if bandage then
+            local clone = bandage:Clone()
+            clone.Parent = player:WaitForChild("Backpack")
+            print("Successfully gave bandage to " .. player.Name)
+        else
+            warn("bandage not found")
+        end
+
+
+        local malungayPandesal = buyToolFolder:FindFirstChild("Malungay Pandesal")
+
+        if malungayPandesal then
+        local clone = malungayPandesal:Clone()
+        clone.Parent = player:WaitForChild("Backpack")
+        print("Successfully gave Malungay Pandesal to " .. player.Name)
+        else
+        warn("Malungay Pandesal not found")
+        end
+
+        
         local malungayPandesal = buyToolFolder:FindFirstChild("Malungay Pandesal")
 
         if malungayPandesal then
