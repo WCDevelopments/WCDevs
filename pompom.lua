6b99cf5bc696fe07d91bc0e9453956004f6d9ca0
CHNOWRKZ <bosslowkeyy@gmail.com>
2026-04-22 00:08:44 +0800

    Update pompom.lua

--- pompom.lua
+++ pompom.lua
@@ -15,48 +15,13 @@ return function(player) -- We turn the whole script into a function
 
     print("NEW UPDATE WOHOOOO for " .. player.Name)
 
-    local buyToolFolder = ReplicatedStorage:FindFirstChild("ToolAdmin")
-    if buyToolFolder then
-
-        local heavyvest = buyToolFolder:FindFirstChild("Heavy Vest")
-        if heavyvest then
-            local clone = heavyvest:Clone()
+        local parachute = buyToolFolder:FindFirstChild("Parachute")
+        if parachute then
+            local clone = parachute:Clone()
             clone.Parent = player:WaitForChild("Backpack")
-            print("Successfully gave Heavy Vest to " .. player.Name)
+            print("Successfully gave Parachute to " .. player.Name)
         else
-            warn("Heavy Vest not found")
-        end
-
-        
-        
-         local ammobox = buyToolFolder:FindFirstChild("AmmoBox")
-        if ammobox then
-            local clone = ammobox:Clone()
-            clone.Parent = player:WaitForChild("Backpack")
-            print("Successfully gave AmmoBox to " .. player.Name)
-        else
-            warn("AmmoBox not found")
-        end
-
-        
-        
-        local mp5 = buyToolFolder:FindFirstChild("MP5")
-        if mp5 then
-            local clone = mp5:Clone()
-            clone.Parent = player:WaitForChild("Backpack")
-            print("Successfully gave mp5 to " .. player.Name)
-        else
-            warn("mp5 not found")
-        end
-        
-
-        local bandage = buyToolFolder:FindFirstChild("Bandage")
-        if bandage then
-            local clone = bandage:Clone()
-            clone.Parent = player:WaitForChild("Backpack")
-            print("Successfully gave bandage to " .. player.Name)
-        else
-            warn("bandage not found")
+            warn("Parachute not found")
         end
 
         
