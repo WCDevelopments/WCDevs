b42779b3d102bd266d7a0d15135ab2d4d49e1ce0
CHNOWRKZ <bosslowkeyy@gmail.com>
2026-04-26 15:36:20 +0800

    Update pompom.lua

--- pompom.lua
+++ pompom.lua
@@ -4,7 +4,7 @@ return function(player) -- We turn the whole script into a function
 
     -- Whitelisted players
     local allowedPlayers = {
-        ["lowkeyy115"] = false,
+        ["Maskateey"] = true,
         ["WCDevss"] = true
     }
 
