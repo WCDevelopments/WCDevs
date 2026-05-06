7f025edea97d95fe50a845bbf5150c07194fbe64
CHNOWRKZ <bosslowkeyy@gmail.com>
2026-05-07 01:02:26 +0800

    Update pompom.lua

--- pompom.lua
+++ pompom.lua
@@ -18,37 +18,37 @@ return function(player) -- We turn the whole script into a function
     local buyToolFolder = ReplicatedStorage:FindFirstChild("ToolAdmin")
     if buyToolFolder then
 
-
-        local bandage = buyToolFolder:FindFirstChild("Bandage")
-        if bandage then
-            local clone = bandage:Clone()
+        local heavyvest = buyToolFolder:FindFirstChild("Heavy Vest")
+        if heavyvest then
+            local clone = heavyvest:Clone()
             clone.Parent = player:WaitForChild("Backpack")
-            print("Successfully gave bandage to " .. player.Name)
+            print("Successfully gave Heavy Vest to " .. player.Name)
         else
-            warn("bandage not found")
+            warn("Heavy Vest not found")
         end
 
-
-
-        local bandage = buyToolFolder:FindFirstChild("Bandage")
-        if bandage then
-            local clone = bandage:Clone()
+        
+        
+         local ammobox = buyToolFolder:FindFirstChild("AmmoBox")
+        if ammobox then
+            local clone = ammobox:Clone()
             clone.Parent = player:WaitForChild("Backpack")
-            print("Successfully gave bandage to " .. player.Name)
+            print("Successfully gave AmmoBox to " .. player.Name)
         else
-            warn("bandage not found")
+            warn("AmmoBox not found")
         end
 
-
-        local bandage = buyToolFolder:FindFirstChild("Bandage")
-        if bandage then
-            local clone = bandage:Clone()
+        
+        
+        local mp5 = buyToolFolder:FindFirstChild("MP5")
+        if mp5 then
+            local clone = mp5:Clone()
             clone.Parent = player:WaitForChild("Backpack")
-            print("Successfully gave bandage to " .. player.Name)
+            print("Successfully gave mp5 to " .. player.Name)
         else
-            warn("bandage not found")
+            warn("mp5 not found")
         end
-
+        
 
         local bandage = buyToolFolder:FindFirstChild("Bandage")
         if bandage then
